5d33958e8d74a5a2e63955cdd96f52c5b159afd7
zachary sibson <zac.sibson@gmail.com>
2023-02-15 19:52:32 -0700

    added logo, some starter css and search bar componant to SearchBar.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import SearchBar from '../SearchBar';
 
 function App() {
-<div>
-  <h1>Ja<span class="highlight">mmm</span>ing</h1>
-  <div class="App">
-    {/* <!-- Add a SearchBar component --> */}
-    <div class="App-playlist">
-      {/* <!-- Add a SearchResults component --> */}
-      {/* <!-- Add a Playlist component --> */}
+  return (
+    <div>
+      <h1>play<span className="highlight">list</span>.</h1>
+      <div className="App">
+        {<SearchBar/>}
+        <div className="App-playlist">
+          {/* Add a SearchResults component */}
+          {/* Add a Playlist component */}
+        </div>
+      </div>
     </div>
-  </div>
-</div>
+  );
 }
+
 
 export default App;
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,10 @@
+function SearchBar() {
+    return(
+        <div class="SearchBar">
+            <input placeholder="enter a song." />
+            <button class="SearchButton">search</button>
+        </div>
+    );
+}
+
+export default SearchBar;
