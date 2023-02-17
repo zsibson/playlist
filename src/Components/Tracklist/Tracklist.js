f6d8a3830d2eb3ab3f9436b26b8cd84d39fd6d78
zachary sibson <zac.sibson@gmail.com>
2023-02-16 21:48:46 -0700

    created Tracklist and Track directorys

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,11 @@
+import './Tracklist.css';
+
+function Tracklist() {
+    return(
+        <div class="TrackList">
+        {/* <!-- Add a map method that renders a set of Track components  --> */}
+        </div>
+    )
+}
+
+export default Tracklist;
