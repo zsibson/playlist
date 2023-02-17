5fa49448cfa9b4164ee21b21d37c1ea74d50e426
zachary sibson <zac.sibson@gmail.com>
2023-02-16 18:00:19 -0700

    added playlist component directory and search results component directory as well as starter css

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,11 @@
+import './Playlist.css';
+
+function Playlist() {
+    return(
+        <div className="Playlist">
+            <input defaultValue={'new playlist'}/>
+            {/* <!-- Add a TrackList component --> */}
+            <button class="Playlist-save">save to spotify 🎉</button>
+        </div>
+    )
+}
---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,10 @@
+import './SearchResults.css';
+
+function SearchResults() {
+    return(
+        <div className="SearchResults">
+            <h2>Results</h2>
+        {/* <!-- Add a TrackList component --> */}
+        </div>
+    );
+}
