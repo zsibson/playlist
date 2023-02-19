e031473d3de3a3206af2027a5f4d51e9ef154e74
zachary sibson <zac.sibson@gmail.com>
2023-02-18 19:08:39 -0700

    changes to the front end

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -5,7 +5,7 @@ class SearchResults extends React.Component {
     render(){
         return(
             <div className="SearchResults">
-                <h2>Results</h2>
+                <h2>results</h2>
             {/* <!-- Add a TrackList component --> */}
             </div>
         );
