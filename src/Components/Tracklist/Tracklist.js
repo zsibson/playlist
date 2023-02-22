315a787052089bb0f918e6851fd39a1d03ff65e6
Zachary Sibson <zacmfp@Zacharys-MacBook-Air.local>
2023-02-22 16:18:17 -0700

    passed props in tracklist and track

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -6,8 +6,15 @@ class Tracklist extends React.Component {
     render(){
         return(
             <div className="TrackList">
-            <Track />
-            
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track}
+                                      key={track.id}
+                                      onAdd={this.props.onAdd}
+                                      isRemoval={this.props.isRemoval}
+                                      onRemove={this.props.onRemove} />
+                    })
+                }
             </div>
         )
     }
