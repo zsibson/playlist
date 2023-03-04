a3153e8e84d6c0e99fbd8ec35d5f4980ee4f1ca1
zachary sibson <zac.sibson@gmail.com>
2023-03-03 18:32:37 -0700

    refractoring code to render hard coded search results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,61 +1,35 @@
-import React  from 'react';
+import {useState}  from 'react';
 import './App.css';
+
 
 // Import Components
 import SearchBar from '../SeachBar/SearchBar';
 import Playlist from '../Playlist/Playlist';
 import SearchResults from '../SearchResults/SearchResults';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchResults: [
+function App() {
+    const [searchResults, setSearchResults] = useState(
+      [
         {
           name: 'Goodness of God',
           artist: 'Bethel Music',
           album: 'Victory',
-          id: Math.random(1000),
-      },
-      {
-        name: 'Goodness of God',
-        artist: 'Bethel Music',
-        album: 'Victory',
-        id: Math.random(1000),
-    },
-    {
-      name: 'Goodness of God',
-      artist: 'Bethel Music',
-      album: 'Victory',
-      id: Math.random(1000),
-  },
-      ],
-      playlistName: 'new playlist',
-      playlistTracks: [
+          id: 1,
+        },
         {
-        name: 'Goodness of God',
-        artist: 'Bethel Music',
-        album: 'Victory',
-        id: Math.random(1000),
-    },
-    {
-      name: 'Goodness of God',
-      artist: 'Bethel Music',
-      album: 'Victory',
-      id: Math.random(1000),
-  },
-  {
-    name: 'Goodness of God',
-    artist: 'Bethel Music',
-    album: 'Victory',
-    id: Math.random(1000),
-},
-  ],
-    }
-    console.log(this.state);
-  }
-  
-  render(){
+          name: 'Victory is Yours',
+          artist: 'Bethel Music',
+          album: 'Victory',
+          id: 2,
+        },
+        {
+          name: 'Raise a Hallelujah',
+          artist: 'Bethel Music',
+          album: 'Victory',
+          id: 3,
+        },
+      ]);
+
     return (
       <div>
         <h1>play<span className="highlight">list</span>.</h1>
@@ -64,7 +38,7 @@ class App extends React.Component {
           <div className="App-playlist">
 
             {/* Search Result Component Below */}
-            {<SearchResults searchResults={this.state.searchResults} />}
+            {<SearchResults searchResults={searchResults} />}
 
             {/* Add a Playlist component */}
             {<Playlist />}
@@ -73,7 +47,6 @@ class App extends React.Component {
         </div>
       </div>
     );
-  }
 }
 
 
--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import Track from '../Track/Track';
 import './Tracklist.css'; 
+
+import Track from '../Track/Track';
 
 class Tracklist extends React.Component {
     render() {
       const tracks = this.props.tracks || [];
-
       return (
         <div className="TrackList">
-          {tracks.map(track => {
+          {
+          tracks.map(track => {
             return (
               <Track
                 track={track}
                 key={track.id}
-                onAdd={this.props.onAdd}
-                isRemoval={this.props.isRemoval}
-                onRemove={this.props.onRemove}
+                // onAdd={this.props.onAdd}
+                // isRemoval={this.props.isRemoval}
+                // onRemove={this.props.onRemove}
               />
             );
           })}
