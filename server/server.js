c541834e4f368ed181876ed7410da7757b920ccd
zachary sibson <zac.sibson@gmail.com>
2023-03-08 23:41:14 -0700

    initial add of authorization code

---
+++ server/server.js
@@ -0,0 +1,4 @@
+//todo
+// authentication setup
+// server route setup
+// get tokens
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,63 +1,17 @@
 import {useState, useCallback}  from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // Import Components
+import Login from '../../utils/Login';
 import SearchBar from '../SeachBar/SearchBar';
 import Playlist from '../Playlist/Playlist';
 import SearchResults from '../SearchResults/SearchResults';
 
 function App() {
   const [playlistName, setPlaylistName] = useState('my playlist');
-  const [playlistTracks, setPlaylistTracks] = useState(
-    [
-      {
-        name: 'Know You Will',
-        artist: 'Hillsong United',
-        album: 'Know You Will',
-        id: 4,
-      },
-      {
-        name: 'Your Love Is Strong',
-        artist: 'Cory Asbury',
-        album: 'Reckless Love',
-        id: 5,
-      },
-      {
-        name: 'Abba',
-        artist: 'John Mark Pantana',
-        album: 'Love Secrets',
-        id: 6,
-      },
-      {
-        name: 'Here I am to Worship',
-        artist: 'Shane and Shane',
-        album: 'Worship Initiative',
-        id: 7,
-      },
-    ]
-  );
-    const [searchResults, setSearchResults] = useState(
-      [
-        {
-          name: 'Goodness of God',
-          artist: 'Bethel Music',
-          album: 'Victory',
-          id: 1,
-        },
-        {
-          name: 'Victory is Yours',
-          artist: 'Bethel Music',
-          album: 'Victory',
-          id: 2,
-        },
-        {
-          name: 'Raise a Hallelujah',
-          artist: 'Bethel Music',
-          album: 'Victory',
-          id: 3,
-        },
-      ]
-    );
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
     
 const addTrack = useCallback(track => {
   if (playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
@@ -85,11 +39,17 @@ const search = (term) => {
 
     return (
       <div>
-        <h1>play<span className="highlight">list</span>.</h1>
+        <div>
+          <Login />
+        </div>
+        {/* <h1>play<span className="highlight">list</span>.</h1>
         <div className="App">
+        
+
           <SearchBar onSearch={search}
            />
           <div className="App-playlist">
+            
 
             <SearchResults searchResults={searchResults}
                            onAdd={addTrack}
@@ -103,7 +63,7 @@ const search = (term) => {
             />
 
           </div>
-        </div>
+        </div> */}
       </div>
     );
 }
